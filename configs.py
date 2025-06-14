4267ae553a2c8b3aa362b19d1b0d7b9c3ffb7f29
ROHIT12-bit <reddyrohit823@gmail.com>
2025-06-14 18:36:36 +0530

    Djdh

--- configs.py
+++ configs.py
@@ -7,7 +7,7 @@ class Config:
     API_HASH = getenv("API_HASH", "72095ec36984aa9ceb0dbaa9cec31559")
     BOT_TOKEN = getenv("BOT_TOKEN", "7242758381:AAGkWrJ1z_0Jrmggh-Bo2D_6036Xj92NX4w")
     # Your Force Subscribe Channel Id Below 
-    CHID = int(getenv("CHID", "-1002611038436")) # Make Bot Admin In This Channel
+    CHID = int(getenv("CHID", "-1002291875883")) # Make Bot Admin In This Channel
     # Admin Or Owner Id Below
     SUDO = list(map(int, getenv("SUDO", "7845335174").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://rohitreddyathuru:R6Co7MOjTYQOAqcq@cluster0.xrwjpl9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
